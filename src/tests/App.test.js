ecc9fd0da9dbbecfd11fd54fa6e67d04717edf1c
GustavoMenezes97 <gustavo.a.r.menezes@gmail.com>
2022-08-19 19:10:14 -0300

    Implementação do teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import renderWithRouter from '../utils/renderWithRouter';
 import App from '../App';
 
@@ -18,7 +19,26 @@ describe('Testa a tela de login', () => {
     renderWithRouter(<App />);
     
     const button = screen.getByRole('button', {name: /enter/i})
-  
+    
     expect(button).toBeInTheDocument();
   });
+  
+  test('Testa o comportamento de habilitar e desabilitar o botão', () => {
+    renderWithRouter(<App />);
+    const email = screen.getByLabelText(/email/i)
+    const password = screen.getByLabelText(/password/i)
+    const button = screen.getByRole('button', {name: /enter/i})
+
+    expect(button).toBeDisabled()
+    
+    userEvent.type(email, "marcelinho")
+    userEvent.type(password, "12345")
+
+    expect(button).toBeDisabled()
+
+    userEvent.type(email, "marcelinho@teste.com")
+    userEvent.type(password, "12345678")
+
+    expect(button).toBeEnabled();
+  })
 })
