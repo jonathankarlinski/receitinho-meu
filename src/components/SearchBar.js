a1de18de98fd5fc330001156cffbccab3ae50459
Peter Fritz <me@peterfritz.dev>
2022-08-22 18:16:36 -0300

    Requisito 90 concluído

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function SearchBar() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div>
-      <img
-        className="searchIcon"
-        src={ searchIcon }
-        data-testid="search-top-btn"
-        alt="Search"
-      />
+      <button
+        type="button"
+        onClick={ () => setVisible(!visible) }
+      >
+        <img
+          className="searchIcon"
+          src={ searchIcon }
+          data-testid="search-top-btn"
+          alt="Search"
+        />
+      </button>
+      {
+        visible && <input
+          type="text"
+          data-testid="search-input"
+        />
+      }
     </div>
   );
 }
