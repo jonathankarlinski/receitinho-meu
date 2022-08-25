1e214c56669864628656fa623136fa9a8aaa92a9
Vinicius <viniciuseduardorr@gmail.com>
2022-08-25 19:23:52 -0300

    Requisito 29

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import renderWithRouter from '../utils/renderWithRouter';
 import RecipeDetails from '../pages/RecipeDetails';
@@ -7,9 +7,13 @@ import RecipeDetails from '../pages/RecipeDetails';
 describe('Testa do componente RecipeDetails', () => {
   test('Testa se existe o componente', () => {
     renderWithRouter(<RecipeDetails />);
-    
-    const text = screen.getByText('Recipe Details')
-  
+
+    const text = screen.getByText('Recomendações')
+
+    waitFor(() => {
+      expect(screen.getByTestId('0-recomendation-card')).toBeInTheDocument()
+    })
+
     expect(text).toBeInTheDocument();
   });
 });
