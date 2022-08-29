6b705dd3af13a1aa5f3050247652f98bd76ad266
Ricardo Gabriel <riicardogabriel@gmail.com>
2022-08-29 19:25:45 -0300

    Inicio dos testes

--- src/tests/RecipeInProgress.test.js
+++ src/tests/RecipeInProgress.test.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import renderWithRouter from '../utils/renderWithRouter';
 import RecipeInProgress from '../pages/RecipeInProgress';
+jest.mock('clipboard-copy');
 
 describe('Testa do componente RecipeInProgress', () => {
-  test('Testa se existe o componente', () => {
+  test('Testa se existe o botão de compartilhar', () => {
     renderWithRouter(<RecipeInProgress />);
     
-    const text = screen.getByText('Recipe In Progress')
+    const btnShare = screen.getByText('Share');
+
+    userEvent.click(btnShare);
   
-    expect(text).toBeInTheDocument();
+    expect(btnShare).toBeInTheDocument();
+  });
+  test('Testa os checks da receita', async () => {
+    const { history } = renderWithRouter(<RecipeInProgress />);
+    
+    history.push("/foods/52977/in-progress");
+    expect(history.location.pathname).toBe("/foods/52977/in-progress");
+
+    await waitFor(() => {
+      expect(screen.getByTestId('0-ingredient-step')).toBeInTheDocument()
+    }, {timeout: 3000})
+
+    // const btnShare = screen.getByText('Shar');
+
+    // userEvent.click(btnShare);
+  
+    // expect(btnShare).toBeInTheDocument();
   });
 });
