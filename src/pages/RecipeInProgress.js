479092d4cd1e65201d0d73c635d1ed149b618c28
Peter Fritz <me@peterfritz.dev>
2022-08-29 15:27:00 -0300

    Requisitos 37 e 38 concluídos

--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { fetchById } from '../services/searchAPI';
 
 export default function RecipeInProgress() {
+  const [recipe, setRecipe] = useState({
+    ingredients: [],
+  });
+
+  const { id } = useParams();
+  const location = useLocation();
+
+  const type = useMemo(() => ({
+    route: location.pathname.includes('foods') ? '/foods' : '/drinks',
+    path: location.pathname.includes('foods') ? 'meal' : 'cocktail',
+    name: location.pathname.includes('foods') ? 'Meal' : 'Drink',
+  }), [location.pathname]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const item = await fetchById(id, type.path);
+
+      console.log(item);
+
+      setRecipe(item);
+    };
+
+    fetchAPI();
+  }, [id]);
+
   return (
-    <div>Recipe In Progress</div>
+    <div>
+      <img
+        data-testid="recipe-photo"
+        src={ recipe[`str${type.name}Thumb`] }
+        alt={ recipe[`str${type.name}`] }
+      />
+      <h1 data-testid="recipe-title">
+        { recipe[`str${type.name}`] }
+      </h1>
+      <button data-testid="share-btn" type="button">
+        Compartilhe
+      </button>
+      <button data-testid="favorite-btn" type="button">
+        Favoritar
+      </button>
+      <p data-testid="recipe-category">
+        { recipe.strCategory }
+        &nbsp;
+        { recipe.strAlcoholic }
+      </p>
+      { recipe.ingredients.map((ingredient, index) => (
+        <div key={ index }>
+          <label
+            htmlFor={ `checkbox-${index}` }
+            data-testid={ `${index}-ingredient-step` }
+          >
+            <p>
+              <strong>{ingredient.name}</strong>
+              &nbsp;
+              <span>{ingredient.qty}</span>
+            </p>
+            <input
+              type="checkbox"
+              id={ `checkbox-${index}` }
+            />
+          </label>
+        </div>
+      )) }
+      <p data-testid="instructions">
+        { recipe.strInstructions }
+      </p>
+      <button data-testid="finish-recipe-btn" type="button">
+        Finalizar Receita
+      </button>
+    </div>
   );
 }
