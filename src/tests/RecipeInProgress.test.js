4e95f40d5b6c6a9005fc311b510d1e2bca73c397
Ricardo Gabriel <riicardogabriel@gmail.com>
2022-08-30 14:21:41 -0300

    Feito testes

--- src/tests/RecipeInProgress.test.js
+++ src/tests/RecipeInProgress.test.js
@@ -3,32 +3,44 @@ import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import renderWithRouter from '../utils/renderWithRouter';
 import RecipeInProgress from '../pages/RecipeInProgress';
+import App from '../App';
+import { SearchProvider } from '../context/search';
 jest.mock('clipboard-copy');
 
 describe('Testa do componente RecipeInProgress', () => {
-  test('Testa se existe o botão de compartilhar', () => {
+  test('Testa se existe o botão de compartilhar', async () => {
     renderWithRouter(<RecipeInProgress />);
     
-    const btnShare = screen.getByText('Share');
+    expect(screen.getByTestId('share-btn')).toBeInTheDocument();
+    expect(screen.getByTestId('favorite-btn')).toBeInTheDocument();
 
+    const btnShare = screen.getByText('Share');
     userEvent.click(btnShare);
-  
-    expect(btnShare).toBeInTheDocument();
   });
   test('Testa os checks da receita', async () => {
-    const { history } = renderWithRouter(<RecipeInProgress />);
+    const { history } = renderWithRouter(
+      <SearchProvider>
+        <App />
+      </SearchProvider>
+    )
     
     history.push("/foods/52977/in-progress");
     expect(history.location.pathname).toBe("/foods/52977/in-progress");
 
-    await waitFor(() => {
-      expect(screen.getByTestId('0-ingredient-step')).toBeInTheDocument()
-    }, {timeout: 3000})
+    const title = await screen.findByText('Corba');
+    expect(title).toBeInTheDocument();
 
-    // const btnShare = screen.getByText('Shar');
+    const ingredient1 = await screen.findByText('Lentils');
+    const ingredient2 = await screen.findByText('Onion');
+    const ingredient3 = await screen.findByText('Mint');
 
-    // userEvent.click(btnShare);
-  
-    // expect(btnShare).toBeInTheDocument();
+    expect(ingredient1).toBeInTheDocument();
+    userEvent.click(ingredient1);
+
+    expect(ingredient2).toBeInTheDocument();
+    userEvent.click(ingredient2);
+
+    expect(ingredient3).toBeInTheDocument();
+    userEvent.click(ingredient3);
   });
 });
